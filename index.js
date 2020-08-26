708ff7fcfdcf6f55785f6375575dcdc9594b9678
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-26 18:25:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ function writeCards(array, event){
   for (let i=0; i<array.length; i++){
    let newarry=[] 
     newarry=newarry.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+   
   }
+   return  newarry
 }
