dba34a7f58aa6a1f87b910f48cf24adae4d5ae0a
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-26 18:35:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, event){
+  let newarry=[] 
+  for (let i=0; i<array.length; i++){
+    newarry[i]=`Thank you, ${array[i]}, for the wonderful ${event} gift!`
+  }
+   return  newarry
+}
+
+function countDown(int){
+  let i=0
+  while(int>=i){
+    console.log(int--)
+  }
+}
+
