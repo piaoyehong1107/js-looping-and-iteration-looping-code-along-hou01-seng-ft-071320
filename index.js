9d4956eb5f9768ac2d08f45ba46ed299a4e52ca3
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-26 18:19:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 
 function writeCards(array, event){
-  for (let i=0; i<arrayname.length; i++){
+  for (let i=0; i<array.length; i++){
    let newarry=[]
    return newarry.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
   }
